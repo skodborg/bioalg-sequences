ba2932cc0304166f5c80b6f80577fdb017715de6
Simon Fischer <mail@simonfischer.com>
2016-09-16 15:56:17 +0200

    implemented parsign

--- project2.py
+++ project2.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+import argparse
+
+
+
+
 alph = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
 
 #         A,  C,  G,  T
@@ -45,12 +50,58 @@ def cost(str_tuple):
         curr_gapcount = 0
     return costsum
 
+def read_input_fasta(aFile):
+    return read_input(aFile)[6:].replace(" ","").replace("\n", "")
+
+def read_input(aFile):
+    f = open(aFile, 'r', encoding='latin1')
+    return f.read()
+
+def read_input_score(aFile):
+    with open(aFile) as f:
+        lines = f.readlines()
+        array = [i.split() for i in lines]
+        cost = array[0][0]
+
+        alphabet = {}
+
+        for i in range(1, len(array)):
+            alphabet[array[i][0]] = (i-1)
+
+        scoreMatrix = [[array[i][x] for x in range(1, len(array[i]))] for i in range(1, len(array))]
+
+        return (cost, alphabet, scoreMatrix)
+
 def main():
-    s1 = 'GGCCTAAAGGCGCCGGTCTTTCGTACCCCAAAATCTCG-GCATTTTAAGATAAGTG-AGTGTTGCGTTACACTAGCGATCTACCGCGTCTTATACT-TAAGCG-TATGCCC-AGATCTGA-CTAATCGTGCCCCCGGATTAGACGGGCTTGATGGGAAAGAACAGCTCGTC---TGTT-TAC--GTATAAACAGAATCGCCTGGGTTCGC'
-    s2 = 'GGGCTAAAGGTTAGGGTCTTTCACACTAAAGAGTGGTGCGTATCGT-GGCTAA-TGTACCGCTTC-TGGTA-TCGTGGCTTA-CG-GCCAGAC-CTACAAGTACTAGACCTGAGAACTAATCTTGTCGAGCCTTC-CATT-GA-GGG--TAATGGGAGAGAACATCGAGTCAGAAGTTATTCTTGTTTACGTAGAATCGCCTGGGTCCGC'
-    # print(cost(('AATAAT', 'AA-GG-')))
-    print(cost((s1, s2)))
+    parser = argparse.ArgumentParser()
     
+
+    input_seq1 = "Path to FASTA file containing the first sequence"
+    parser.add_argument("seq1", help=input_seq1)
+    input_seq2 = "Path to FASTA file containing the second sequence"
+    parser.add_argument("seq2", help=input_seq2)
+    
+    input_scoreMatrix = "Path to a file containing the score matrix"
+    parser.add_argument("scoreMatrix", help=input_scoreMatrix)
+    
+
+    args = parser.parse_args()
+
+
+    seq1 = read_input_fasta(args.seq1)
+    seq2 = read_input_fasta(args.seq2)
+
+    
+    scoreMatrixTotal = (cost, alphabet, scoreMatrix) = read_input_score(args.scoreMatrix)
+
+    #s1 = 'GGCCTAAAGGCGCCGGTCTTTCGTACCCCAAAATCTCG-GCATTTTAAGATAAGTG-AGTGTTGCGTTACACTAGCGATCTACCGCGTCTTATACT-TAAGCG-TATGCCC-AGATCTGA-CTAATCGTGCCCCCGGATTAGACGGGCTTGATGGGAAAGAACAGCTCGTC---TGTT-TAC--GTATAAACAGAATCGCCTGGGTTCGC'
+    #s2 = 'GGGCTAAAGGTTAGGGTCTTTCACACTAAAGAGTGGTGCGTATCGT-GGCTAA-TGTACCGCTTC-TGGTA-TCGTGGCTTA-CG-GCCAGAC-CTACAAGTACTAGACCTGAGAACTAATCTTGTCGAGCCTTC-CATT-GA-GGG--TAATGGGAGAGAACATCGAGTCAGAAGTTATTCTTGTTTACGTAGAATCGCCTGGGTCCGC'
+    #print(cost(('AATAAT', 'AA-GG-')))
+    #print(cost((s1, s2)))
+
+
+    
+
 
 if __name__ == '__main__':
     main()
