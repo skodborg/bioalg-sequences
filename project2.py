21d7251c484b29a23ca3131363432b9c6a906f4c
Simon Fischer <mail@simonfischer.com>
2016-09-19 14:59:24 +0200

    added tests

--- project2.py
+++ project2.py
@@ -102,7 +102,7 @@ def cost(str_tuple):
     return costsum
 
 def read_input_fasta(aFile):
-    return read_input(aFile)[6:].replace(" ","").replace("\n", "")
+    return read_input(aFile)[6:].replace(" ","").replace("\n", "").upper()
 
 def read_input(aFile):
     f = open(aFile, 'r', encoding='latin1')
@@ -112,18 +112,20 @@ def read_input_score(aFile):
     with open(aFile) as f:
         lines = f.readlines()
         array = [i.split() for i in lines]
-        cost = array[0][0]
+        cost = (alphaCost, betaCost) = (int(array[0][0]), int(array[0][1]))
 
         alphabet = {}
 
         for i in range(1, len(array)):
             alphabet[array[i][0]] = (i-1)
 
-        scoreMatrix = [[array[i][x] for x in range(1, len(array[i]))] for i in range(1, len(array))]
+        scoreMatrix = [[int(array[i][x]) for x in range(1, len(array[i]))] for i in range(1, len(array))]
 
         return (cost, alphabet, scoreMatrix)
 
 def main():
+    global mSub, alpha, beta
+    
     parser = argparse.ArgumentParser()
 
     input_seq1 = "Path to FASTA file containing the first sequence"
@@ -140,11 +142,20 @@ def main():
 
     seq1 = read_input_fasta(args.seq1)
     seq2 = read_input_fasta(args.seq2)
-
     
-    scoreMatrixTotal = (cost, alphabet, scoreMatrix) = read_input_score(args.scoreMatrix)
 
-    print(optimal_cost(seq1, seq2))
+    for i in range(1, 2):
+    
+        scoreMatrixTotal = ((alphaCost, betaCost), alphabet, scoreMatrix) = read_input_score("project_2_examples/scorematrix_1.txt")
+        seq1 = read_input_fasta("project_2_examples/seq1_ex" + str(i) + ".txt")
+        seq2 = read_input_fasta("project_2_examples/seq2_ex" + str(i) + ".txt")
+        alpha = alphaCost
+        beta = betaCost
+        mSub = scoreMatrix
+        print(i)
+        print(optimal_cost(seq1, seq2))
+    
+    
 
 
 if __name__ == '__main__':
