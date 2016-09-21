338970ec250d453da034af640f57c9e8988c4b76
skodborg <skod92@gmail.com>
2016-09-21 18:49:47 +0200

    added interface for global alignment with affine cost function to the clumsy project2.py file for easier use

---
+++ global_affine.py
@@ -0,0 +1,43 @@
+import argparse
+import project2 as p2
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    input_seq1 = "Path to FASTA file containing the first sequence"
+    parser.add_argument("seq1", help=input_seq1)
+    input_seq2 = "Path to FASTA file containing the second sequence"
+    parser.add_argument("seq2", help=input_seq2)
+    input_scoreMatrix = "Path to a file containing the score matrix"
+    parser.add_argument("scoreMatrix", help=input_scoreMatrix)
+    input_alpha = "alpha value to be used in affine cost function"
+    parser.add_argument("alpha", help=input_alpha, type=int)
+    input_beta = "beta value to be used in affine cost function"
+    parser.add_argument("beta", help=input_beta, type=int)
+    input_backtrack = "returns an optimal alignment along with the cost"
+    parser.add_argument('-b', '--backtrack', help=input_backtrack, action='store_true')
+
+    args = parser.parse_args()
+
+    seq1 = p2.read_input_fasta(args.seq1)
+    seq2 = p2.read_input_fasta(args.seq2)
+
+    alphabet, scoreMatrix = p2.read_input_score(args.scoreMatrix)
+
+    alphaCost = args.alpha
+    betaCost = args.beta
+    bool_backtrack = args.backtrack
+
+    p2.alpha = alphaCost
+    p2.beta = betaCost
+    p2.mSub = scoreMatrix  
+    p2.alph = alphabet
+
+    cost, alignment = p2.optimal_cost(seq1, seq2, min, bool_backtrack)
+    print('cost: %i' % cost)
+    if bool_backtrack:
+        print('alignment: %s' % (str(alignment)))
+
+
+if __name__ == '__main__':
+    main()
