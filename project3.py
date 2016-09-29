a2ebd1879d8c02976bde03faecd130cc1d4b7c80
skodborg <skod92@gmail.com>
2016-09-29 13:15:02 +0200

    prettified a bit

--- project3.py
+++ project3.py
@@ -1,6 +1,6 @@
-import numpy as np
 import argparse
 import project2 as prj2
+
 
 def sp_func(substmatrix, gapcost, alph):
     def hasGap(char):
@@ -15,7 +15,7 @@ def sp_func(substmatrix, gapcost, alph):
             return substmatrix[alph[char1]][alph[char2]] + \
                    substmatrix[alph[char2]][alph[char3]] + \
                    substmatrix[alph[char1]][alph[char3]]
-        
+
         if(not hasGap(char1) and not hasGap(char2) and hasGap(char3)):
             # return sub(A[i], B[j]) + gap + gap
             return substmatrix[alph[char1]][alph[char2]] + gapcost + gapcost
@@ -25,93 +25,105 @@ def sp_func(substmatrix, gapcost, alph):
             return gapcost + substmatrix[alph[char1]][alph[char3]] + gapcost
 
         if(hasGap(char1) and not hasGap(char2) and not hasGap(char3)):
-            #return gap + gap + sub(B[j], C[k])
+            # return gap + gap + sub(B[j], C[k])
             return gapcost + gapcost + substmatrix[alph[char2]][alph[char3]]
-
 
         return gapcost + gapcost
 
     return returnFunction
 
+
 def recursive_msa(seq1, seq2, seq3, substmatrix, gapcost, alphabet):
+    len1 = range(len(seq1) + 1)
+    len2 = range(len(seq2) + 1)
+    len3 = range(len(seq3) + 1)
     sp = sp_func(substmatrix, gapcost, alphabet)
-    len1 = range(len(seq1))
-    len2 = range(len(seq2))
-    len3 = range(len(seq3))
     T = [[[None for _ in len3] for _ in len2] for _ in len1]
 
     def rec_helper(i, j, k):
+        # memoization
         if T[i][j][k]:
             return T[i][j][k]
 
+        # base case
         if i == 0 and j == 0 and k == 0:
             return 0
 
-        v0 = v1 = v2 = v3 = v4 = v5 = v6 = v7 = float('inf')
-
+        # recursive cases
+        v0 = v1 = v2 = v3 = v4 = v5 = v6 = float('inf')
         if i > 0 and j > 0 and k > 0:
-            v0 = rec_helper(i - 1, j - 1, k - 1) + sp(seq1[i-1], seq2[j-1], seq3[k-1])
+            rec_call = rec_helper(i - 1, j - 1, k - 1)
+            v0 = rec_call + sp(seq1[i - 1], seq2[j - 1], seq3[k - 1])
         if i > 0 and j > 0 and k >= 0:
-            v1 = rec_helper(i - 1, j - 1, k) + sp(seq1[i-1], seq2[j-1], '-')
+            rec_call = rec_helper(i - 1, j - 1, k)
+            v1 = rec_call + sp(seq1[i - 1], seq2[j - 1], '-')
         if i > 0 and j >= 0 and k > 0:
-            v2 = rec_helper(i - 1, j, k - 1) + sp(seq1[i-1], '-', seq3[k-1])
+            rec_call = rec_helper(i - 1, j, k - 1)
+            v2 = rec_call + sp(seq1[i - 1], '-', seq3[k - 1])
         if i >= 0 and j > 0 and k > 0:
-            v3 = rec_helper(i, j - 1, k - 1) + sp('-', seq2[j-1], seq3[k-1])
+            rec_call = rec_helper(i, j - 1, k - 1)
+            v3 = rec_call + sp('-', seq2[j - 1], seq3[k - 1])
         if i > 0 and j >= 0 and k >= 0:
-            v4 = rec_helper(i - 1, j, k) + sp(seq1[i-1], '-', '-')
+            rec_call = rec_helper(i - 1, j, k)
+            v4 = rec_call + sp(seq1[i - 1], '-', '-')
         if i >= 0 and j > 0 and k >= 0:
-            v5 = rec_helper(i, j - 1, k) + sp('-', seq2[j-1], '-')
+            rec_call = rec_helper(i, j - 1, k)
+            v5 = rec_call + sp('-', seq2[j - 1], '-')
         if i >= 0 and j >= 0 and k > 0:
-            v6 = rec_helper(i, j, k - 1) + sp('-', '-', seq3[k-1])
-            
+            rec_call = rec_helper(i, j, k - 1)
+            v6 = rec_call + sp('-', '-', seq3[k - 1])
+
         min_val = min(v0, v1, v2, v3, v4, v5, v6)
         T[i][j][k] = min_val
         return min_val
 
+    print(rec_helper(len(seq1), len(seq2), len(seq3)))
 
 
 def msa(seq1, seq2, seq3, substmatrix, gapcost, alphabet):
-    len1 = range(len(seq1)+1)
-    len2 = range(len(seq2)+1)
-    len3 = range(len(seq3)+1)
+    len1 = range(len(seq1) + 1)
+    len2 = range(len(seq2) + 1)
+    len3 = range(len(seq3) + 1)
     sp = sp_func(substmatrix, gapcost, alphabet)
-
     T = [[[None for _ in len3] for _ in len2] for _ in len1]
 
     for i in len1:
         for j in len2:
             for k in len3:
                 v0 = v1 = v2 = v3 = v4 = v5 = v6 = v7 = float('inf')
-
                 if i == 0 and j == 0 and k == 0:
                     v0 = 0
                 if i > 0 and j > 0 and k > 0:
-                    v1 = T[i - 1][j - 1][k - 1] + sp(seq1[i-1], seq2[j-1], seq3[k-1])
+                    tbl_lookup = T[i - 1][j - 1][k - 1]
+                    v1 = tbl_lookup + sp(seq1[i - 1], seq2[j - 1], seq3[k - 1])
                 if i > 0 and j > 0 and k >= 0:
-                    v2 = T[i - 1][j - 1][k] + sp(seq1[i-1], seq2[j-1], '-')
+                    tbl_lookup = T[i - 1][j - 1][k]
+                    v2 = tbl_lookup + sp(seq1[i - 1], seq2[j - 1], '-')
                 if i > 0 and j >= 0 and k > 0:
-                    v3 = T[i - 1][j][k - 1] + sp(seq1[i-1], '-', seq3[k-1])
+                    tbl_lookup = T[i - 1][j][k - 1]
+                    v3 = tbl_lookup + sp(seq1[i - 1], '-', seq3[k - 1])
                 if i >= 0 and j > 0 and k > 0:
-                    v4 = T[i][j - 1][k - 1] + sp('-', seq2[j-1], seq3[k-1])
+                    tbl_lookup = T[i][j - 1][k - 1]
+                    v4 = tbl_lookup + sp('-', seq2[j - 1], seq3[k - 1])
                 if i > 0 and j >= 0 and k >= 0:
-                    v5 = T[i - 1][j][k] + sp(seq1[i-1], '-', '-')
+                    tbl_lookup = T[i - 1][j][k]
+                    v5 = tbl_lookup + sp(seq1[i - 1], '-', '-')
                 if i >= 0 and j > 0 and k >= 0:
-                    v6 = T[i][j - 1][k] + sp('-', seq2[j-1], '-')
+                    tbl_lookup = T[i][j - 1][k]
+                    v6 = tbl_lookup + sp('-', seq2[j - 1], '-')
                 if i >= 0 and j >= 0 and k > 0:
-                    v7 = T[i][j][k - 1] + sp('-', '-', seq3[k-1])
+                    tbl_lookup = T[i][j][k - 1]
+                    v7 = tbl_lookup + sp('-', '-', seq3[k - 1])
                 T[i][j][k] = min(v0, v1, v2, v3, v4, v5, v6, v7)
-    
-    print(np.array(T).shape)
-    
-    lastx = len(seq1)
-    lasty = len(seq2)
-    lastz = len(seq3)
-    print(T[lastx][lasty][lastz])
+    last1 = len(seq1)
+    last2 = len(seq2)
+    last3 = len(seq3)
+    print(T[last1][last2][last3])
 
 
 def run_tests(seq1, seq2, seq3, substmatrix, alphabet):
     msa(seq1, seq2, seq3, substmatrix, 5, alphabet)
-    #recursive_msa(seq1, seq2, seq3, substmatrix, 5, alphabet)
+    recursive_msa(seq1, seq2, seq3, substmatrix, 5, alphabet)
 
 
 def test_sp(substmatrix, alphabet):
@@ -133,19 +145,19 @@ def test_sp(substmatrix, alphabet):
 
     # Check C GAP; sub(A[i], B[j]) + gap + gap
     #              sub("G", "C") + gap + gap
-    #              5 + 5 + 5 
+    #              5 + 5 + 5
     assert(sp("G", "C", "-") == 15)
     assert(sp('A', 'A', '-') == 10)
 
     # Check B GAP; gap + sub(A[i], C[k]) + gap
     #              gap + sub("G", "A") + gap
-    #              5 + 2 + 5 
+    #              5 + 2 + 5
     assert(sp("G", "-", "A") == 12)
     assert(sp("G", "-", "G") == 10)
 
     # Check A GAP; gap + gap + sub(B[j], C[k])
     #              gap + gap + sub("C", "G")
-    #              5 + 5 + 5 
+    #              5 + 5 + 5
     assert(sp("-", "C", "G") == 15)
     assert(sp("-", "C", "C") == 10)
 
@@ -155,7 +167,6 @@ def test_sp(substmatrix, alphabet):
     assert(sp("-", "-", "G") == 10)
     assert(sp("A", "-", "-") == 10)
     assert(sp("-", "-", "-") == 0)
-
 
 
 def main():
@@ -176,19 +187,18 @@ def main():
     # seq2 = read_input_fasta(args.seq2)
     # seq3 = read_input_fasta(args.seq3)
 
-    #seq1 = 'GTTCCGAAAGGCTAGCGCTAGGCGCC'
-    #seq2 = 'ATGGATTTATCTGCTCTTCG'
-    #seq3 = 'TGCATGCTGAAACTTCTCAACCA'
+    seq1 = 'GTTCCGAAAGGCTAGCGCTAGGCGCC'
+    seq2 = 'ATGGATTTATCTGCTCTTCG'
+    seq3 = 'TGCATGCTGAAACTTCTCAACCA'
 
-    seq1 = 'GTTCCGAAAGGCTAGCGCTAGGCGCCAAGCGGCCGGTTTCCTTGGCGACGGAGAGCGCGGGAATTTTAGATAGATTGTAATTGCGGCTGCGCGGCCGCTGCCCGTGCAGCCAGAGGATCCAGCACCTCTCTTGGGGCTTCTCCGTCCTCGGCGCTTGGAAGTACGGATCTTTTTTCTCGGAGAAAAGTTCACTGGAACTG'
-    seq2 = 'ATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAAAATGTCATTAACGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTCTGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTGCATGCTGAAACTTCTCAACCAGAAGAAAGGGCCTTCACAGTGTCCTTTATGTAAGAATGA'
-    seq3 = 'CGCTGGTGCAACTCGAAGACCTATCTCCTTCCCGGGGGGGCTTCTCCGGCATTTAGGCCTCGGCGTTTGGAAGTACGGAGGTTTTTCTCGGAAGAAAGTTCACTGGAAGTGGAAGAAATGGATTTATCTGCTGTTCGAATTCAAGAAGTACAAAATGTCCTTCATGCTATGCAGAAAATCTTGGAGTGTCCAATCTGTTT'
+    # seq1 = 'GTTCCGAAAGGCTAGCGCTAGGCGCCAAGCGGCCGGTTTCCTTGGCGACGGAGAGCGCGGGAATTTTAGATAGATTGTAATTGCGGCTGCGCGGCCGCTGCCCGTGCAGCCAGAGGATCCAGCACCTCTCTTGGGGCTTCTCCGTCCTCGGCGCTTGGAAGTACGGATCTTTTTTCTCGGAGAAAAGTTCACTGGAACTG'
+    # seq2 = 'ATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAAAATGTCATTAACGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTCTGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTGCATGCTGAAACTTCTCAACCAGAAGAAAGGGCCTTCACAGTGTCCTTTATGTAAGAATGA'
+    # seq3 = 'CGCTGGTGCAACTCGAAGACCTATCTCCTTCCCGGGGGGGCTTCTCCGGCATTTAGGCCTCGGCGTTTGGAAGTACGGAGGTTTTTCTCGGAAGAAAGTTCACTGGAAGTGGAAGAAATGGATTTATCTGCTGTTCGAATTCAAGAAGTACAAAATGTCCTTCATGCTATGCAGAAAATCTTGGAGTGTCCAATCTGTTT'
 
     alphabet, substmatrix = prj2.read_input_score(args.substmatrix)
-    
+
     test_sp(substmatrix, alphabet)
     run_tests(seq1, seq2, seq3, substmatrix, alphabet)
-
 
 
 if __name__ == '__main__':
